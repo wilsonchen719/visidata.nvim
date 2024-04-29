d9818739332437aa91eff084ceb98ea31cb69ac6
wilsonchen719 <145998062+wilsonchen719@users.noreply.github.com>
2024-04-29 10:44:12 +0800

    Back to original way to select visual

--- lua/visidata.lua
+++ lua/visidata.lua
@@ -4,8 +4,8 @@ local M = {}
 local dap = require("dap")
 
 local function get_visual_selection()
-    local _, line_start, col_start = unpack(vim.fn.getpos("<"))
-    local _, line_end, col_end = unpack(vim.fn.getpos(">"))
+    local _, line_start, col_start = unpack(vim.fn.getpos("v"))
+    local _, line_end, col_end = unpack(vim.fn.getpos("."))
     local selection = vim.api.nvim_buf_get_text(0, line_start - 1, col_start - 1, line_end - 1, col_end, {})
     return selection
 end
